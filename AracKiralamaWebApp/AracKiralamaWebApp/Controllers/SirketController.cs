53b530fe0284702b849e95eb786ee7daa60b2989
akın çam <sabricam2016@hotmail.com>
2019-04-30 15:12:46 +0300

    Projeye Ait Web servis ve Web Uygulamaları oluşturuldu

---
+++ AracKiralamaWebApp/AracKiralamaWebApp/Controllers/SirketController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebService;
+using Model.DTOs;
+namespace AracKiralamaWebApp.Controllers
+{
+    public class SirketController : Controller
+    {
+        // GET: Sirket
+        public ActionResult Index()
+        {
+            using(WebService.SirketWebService sirketWebService = new SirketWebService())
+            {
+                var model = sirketWebService.HelloWorld();
+                return View(model);
+
+            }
+        }
+    }
+}
---
+++ AracKiralamaWebServis/WebService/SirketWebService.asmx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+using BusinessLayerLogic.BLLs;
+using Model.DTOs;
+namespace WebService
+{
+    /// <summary>
+    /// Summary description for SirketWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class SirketWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public List<SirketDTO>  HelloWorld()
+        {
+            //BusinessLayerLogic.BLLs.SirketBLL sirketbll = new BusinessLayerLogic.BLLs.SirketBLL();
+            SirketBLL sirket = new SirketBLL(); 
+            return sirket.Get();
+        }
+    }
+}
